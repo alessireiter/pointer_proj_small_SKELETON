5e8e4fab3e55643b85387cafd112f31cabb60b90
Alessi Reiter <alessi.reiter.18@cnu.edu>
2021-03-18 12:46:31 -0400

    final push

--- parser/stringmanip.cpp
+++ parser/stringmanip.cpp
@@ -30,7 +30,7 @@ namespace KP{
 	 * \return
 	 */
 	int amountOfMemoryToAllocateForNewString(int len_src, int numbTagsToReplace, int len_tag, int len_tag_replacement){
-		return len_src - (numbTagsToReplace * len_tag) + (numbTagsToReplace * len_tag_replacement);
+		return len_src - (numbTagsToReplace * len_tag) + (numbTagsToReplace * len_tag_replacement) + 1;
 	}
 
 	/**
@@ -59,7 +59,7 @@ namespace KP{
 			return INVALID_NULL_PTR_DETECTED;
 		}
 
-		int src_len = strlen(src);
+		int src_len = strlen(src) + 1;
 		int tag_len = strlen(tag);
 		int rtag_len = strlen(tag_replacement);
 
@@ -67,13 +67,12 @@ namespace KP{
 		int i = 0;
 		int change = 0;
 		int delta = 0;
-		while (i < src_len){
-			if(*(src+i) == tag[0]) {
-				if (strncmp((src + i), tag, tag_len) == 0) {
 
-					for (int j = 0; j < rtag_len; j++) {
-						*((new_src + i) + j) = *(tag_replacement + j);
-					}
+		while (i + delta < src_len){
+			cout << "i:" << i << "\t:\'" << *((src + i)) << "new_src" << new_src << endl;
+			if(*(src+i+delta) == tag[0]) {
+				if (strncmp((src + i) + delta, tag, tag_len) == 0) {
+					strncat((new_src + i) + change, tag_replacement, rtag_len);
 					change += rtag_len;
 					delta += tag_len;
 				}
@@ -106,7 +105,7 @@ namespace KP{
 				if (*(src + i) == tag[0]) {
 					//found an a, lets see if it could be the start of a beautiful thing
 					if (strncmp((src + i), tag, lenTag) == 0) {
-						//got it whohoo, now what?
+						//got it woohoo, now what?
 						cout << "Found \'"<<tag<<"\' in \'" << src << "\'" << endl;
 						count++;
 					}
--- src/pointer_proj_small.cpp
+++ src/pointer_proj_small.cpp
@@ -24,43 +24,41 @@ int main(int argc, char *argv[]) {
 	} else if (argc != EXPECTED_NUMBER_ARGUMENTS) {
 		cout << HELP_STRING2 << endl;
 		return FAIL_WRONG_NUMBER_ARGS;
-	} else {
-		string input = argv[1]; // input
-		string output = argv[2]; // output
-		string tag = argv[3]; // tag
-		string replacement = argv[4]; // replacement token
-
-		string contents = "";
-		readFile(input, contents);
-
-		char *pcontents = &contents[0];
-		char *ptag = &tag[0];
-		char *pReplacement = &replacement[0];
-		int cnt = KP::findNumbOccurrences(pcontents, ptag);
-
-		int amount = KP::amountOfMemoryToAllocateForNewString(contents.length(), cnt, tag.length(), replacement.length());
-
-		char *pheap = new char[amount];
-		memset(pheap, 0, amount * sizeof(char));
-
-		int replace = KP::replace(pcontents, pheap, ptag, pReplacement);
-		if (replace != SUCCESS) {
-			return replace;
-		}
-
-		int write = writeFile(output, pheap);
-		if (write != SUCCESS) {
-			return write;
-		}
-
-		if (pheap) {
-			delete[] pheap;
-			pheap = 0;
-		}
-
-		return SUCCESS;
-
 	}
+	string input = argv[1]; // input
+	string output = argv[2]; // output
+	string tag = argv[3]; // tag
+	string replacement = argv[4]; // replacement token
+
+	string contents = "";
+	readFile(input, contents);
+
+	char *pcontents = &contents[0];
+	char *ptag = &tag[0];
+	char *pReplacement = &replacement[0];
+	int cnt = KP::findNumbOccurrences(pcontents, ptag);
+
+	int amount = KP::amountOfMemoryToAllocateForNewString(contents.length(), cnt, tag.length(), replacement.length());
+
+	char *pheap = new char[amount];
+	memset(pheap, 0, amount * sizeof(char));
+
+	int replace = KP::replace(pcontents, pheap, ptag, pReplacement);
+	if (replace != SUCCESS) {
+		return replace;
+	}
+
+	int write = writeFile(output, pheap);
+	if (write != SUCCESS) {
+		return write;
+	}
+
+	if (pheap) {
+		delete[] pheap;
+		pheap = 0;
+	}
+
+	return SUCCESS;
 
 
 }
